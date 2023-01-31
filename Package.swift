6b23c726284f7d3f520f200e4119a64114c6807d
Yonas Kolb <yonaskolb@users.noreply.github.com>
2023-01-31 14:04:40 +1100

    cleanup of dependencies

--- Package.swift
+++ Package.swift
@@ -10,25 +10,20 @@ let package = Package(
         .library(name: "SwiftComponent", targets: ["SwiftComponent"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.6.0"),
+        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.7.0"),
         .package(url: "https://github.com/yonaskolb/SwiftGUI", from: "0.2.2"),
-        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.1.2"),
-        .package(url: "https://github.com/pointfreeco/swiftui-navigation", from: "0.4.5"),
-        .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.10.0"),
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.1.4"),
         .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.10.0"),
         .package(url: "https://github.com/Sherlouk/AccessibilitySnapshot", branch: "update-snapshot-testing"),
         .package(url: "https://github.com/wickwirew/Runtime", from: "2.2.4"),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", branch: "develop"),
     ],
     targets: [
         .target(
             name: "SwiftComponent",
             dependencies: [
                 .product(name: "CustomDump", package: "swift-custom-dump"),
-                .product(name: "CasePaths", package: "swift-case-paths"),
                 .product(name: "Parsing", package: "swift-parsing"),
                 .product(name: "Dependencies", package: "swift-dependencies"),
-                .product(name: "SwiftUINavigation", package: "swiftui-navigation"),
                 "SwiftGUI",
                 "SwiftPreview",
                 "Runtime",
