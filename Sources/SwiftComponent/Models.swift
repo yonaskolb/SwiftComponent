3bc07203f557605a305f8ef72450b0f9aaaed360
Yonas Kolb <yonaskolb@users.noreply.github.com>
2023-06-25 17:25:07 +1000

    model extensions

---
+++ Sources/SwiftComponent/Models.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+//typealias SimpleModel<State, Action, Environment> = ViewModel<SimpleComponentModel<State, Action, Environment>>
+//struct SimpleComponentModel<State, Output, Environment>: ComponentModel {
+//
+//    typealias State = State
+//    typealias Action = Output
+//    typealias Output = Output
+//    typealias Environment = Environment
+//
+//    static func handle(action: Action, model: Model) async {
+//        model.output(action)
+//    }
+//}
+
+public protocol ActionOutput: ComponentModel where Action == Output {}
+public extension ActionOutput {
+
+    func handle(action: Action, model: Model) async {
+        model.output(action)
+    }
+}
