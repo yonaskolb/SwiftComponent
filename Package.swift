51d2c8a9abcb14a231619460c0ee2149a0ebb285
Yonas Kolb <yonaskolb@users.noreply.github.com>
2023-09-18 21:35:52 +1000

    update dependencies

--- Package.swift
+++ Package.swift
@@ -10,10 +10,10 @@ let package = Package(
         .library(name: "SwiftComponent", targets: ["SwiftComponent"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.7.0"),
         .package(url: "https://github.com/yonaskolb/SwiftGUI", from: "0.2.2"),
+        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0"),
         .package(url: "https://github.com/yonaskolb/swift-dependencies", branch: "merging"),
-        .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.14.0"),
+        .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.0.0"),
         .package(url: "https://github.com/cashapp/AccessibilitySnapshot", from: "0.6.0"),
         .package(url: "https://github.com/wickwirew/Runtime", from: "2.2.4"),
         .package(url: "https://github.com/apple/swift-syntax", branch: "main")
