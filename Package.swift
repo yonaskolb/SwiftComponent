ed2f6cf4387f9fad1e947963ea53c3e0100de666
Yonas Kolb <yonaskolb@users.noreply.github.com>
2023-12-12 19:53:09 +1100

    update case paths

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .package(url: "https://github.com/yonaskolb/SwiftGUI", from: "0.2.2"),
         .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0"),
         .package(url: "https://github.com/yonaskolb/swift-dependencies", branch: "merging"),
-        .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.1.0"),
         .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.2.1"),
         .package(url: "https://github.com/cashapp/AccessibilitySnapshot", from: "0.6.0"),
         .package(url: "https://github.com/wickwirew/Runtime", from: "2.2.4"),
