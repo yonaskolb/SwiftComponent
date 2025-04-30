26875452884ebabf1df20373864d73707fece022
Yonas Kolb <yonaskolb@users.noreply.github.com>
2025-04-30 21:47:09 +1000

    copyable environments

--- Sources/SwiftComponent/ComponentEnvironment.swift
+++ Sources/SwiftComponent/ComponentEnvironment.swift
@@ -4,9 +4,12 @@ public protocol ComponentEnvironment {
     associatedtype Parent
     var parent: Parent { get }
     static var preview: Self { get }
+    /// provide a copy of the environment. If this is a class it must be a new instance. This is used for snapshots and test branch resets
+    func copy() -> Self
 }
 
 public struct EmptyEnvironment: ComponentEnvironment {
     public var parent: Void = ()
     public static var preview: EmptyEnvironment { .init() }
+    public func copy() -> EmptyEnvironment { .init() }
 }
